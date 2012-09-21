c2e1f491da7c8714653199733ebc6f09aadf5081
Jeff Tucker <jtucker@damballa.com>
2012-09-21 11:58:58 -0400

    Mmmmm... fancy cel shading

--- js/models/prefab.js
+++ js/models/prefab.js
@@ -1,4 +1,10 @@
 define(function(require) {
+
+  // Sourced from http://www.ro.me/tech/cel-shader
+  //
+	var fragmentShader = require('text!shaders/cel.frag');
+	var vertexShader   = require('text!shaders/cel.vert');
+
   function Prefab() { Backbone.Model.apply(this, arguments) };
   return Backbone.Model.extend({
     constructor: Prefab,
@@ -18,7 +24,16 @@ define(function(require) {
           };
 
       var geometry = factory(this);
-      var material = new THREE.MeshBasicMaterial({ color: color, wireframe: wireframe });
+      var material = new THREE.ShaderMaterial({
+				uniforms: {
+					"uDirLightPos":       { type:"v3", value: new THREE.Vector3() },
+					"uDirLightColor":     { type:"c",  value: new THREE.Color(0xeeeeee) },
+					"uAmbientLightColor": { type:"c",  value: new THREE.Color(0x050505) },
+					"uBaseColor":         { type:"c",  value: new THREE.Color(color) }
+				},
+				vertexShader: vertexShader,
+				fragmentShader: fragmentShader
+			});
 
       this.set({ geometry: geometry, material: material }, { silent: true });
     }
